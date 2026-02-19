6e680981cad0242622bb454b09bbf3e5a0d30221
suywho <suy.nepal@gmail.com>
2026-02-19 12:50:42 +0545

    fix: make HabiticaHome stateful for handling BottomNavBar views

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,31 @@
 import "package:flutter/material.dart";
 import "package:habitica_clone/views/todo.dart";
+import "../components/navigation_bar.dart";
 
-class HabiticaHome extends StatelessWidget {
+class HabiticaHome extends StatefulWidget {
   const HabiticaHome({super.key});
+
+  State<HabiticaHome> createState() => _HabiticaHomeState();
+}
+
+class _HabiticaHomeState extends State<HabiticaHome> {
+  int index = 0;
+  static List<Widget> navigationViews = [HomeView(), TodoView()];
+  Widget currentView = Container();
+
+  void onTapHandler(int value) {
+    setState(() {
+      index = value;
+      currentView = navigationViews[index];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentView = navigationViews[index];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,15 +37,25 @@ class HabiticaHome extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
       ),
-
-      body: TodoView(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.checklist), label: "Todos"),
-        ],
+      bottomNavigationBar: BottomNavBar(onTapHandler: onTapHandler),
+      body: currentView,
+      floatingActionButton: ElevatedButton(
+        onPressed: () => {},
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.add, size: 48),
+        ),
       ),
     );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("Home Shi")));
   }
 }
 
